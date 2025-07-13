97ed0e0fa75d6d4419463b15da2ae8510c63d5a9
haristotle <haristotlepatil2003@gmail.com>
2025-07-13 23:49:13 +0530

    Implement WeatherInformation class

--- src/weatherAPI.js
+++ src/weatherAPI.js
@@ -1,5 +1,66 @@
 const vcAPIkey = `3MLWADDZA3VLRW3THLLEXTEUC`;
 
+class WeatherInformation {
+  constructor(weatherJsonObject) {
+    this.icon = weatherJsonObject.currentConditions.icon;
+    this.temp = weatherJsonObject.currentConditions.temp;
+    this.humidity = weatherJsonObject.currentConditions.humidity;
+    this.dew = weatherJsonObject.currentConditions.dew;
+    this.condition = weatherJsonObject.currentConditions.conditions;
+    this.sunriseTime = weatherJsonObject.currentConditions.sunrise;
+    this.sunsetTime = weatherJsonObject.currentConditions.sunset;
+    this.windspeed = weatherJsonObject.currentConditions.windspeed;
+    this.cloudcover = weatherJsonObject.currentConditions.cloudcover;
+    this.weekArr = this.constructWeekArr(weatherJsonObject);
+
+    console.log(this);
+  }
+
+  constructWeekArr(weatherJsonObject) {
+    const weekArr = [];
+
+    for (let i = 0; i < 7; i++) {
+      weekArr.push({
+        datetime: weatherJsonObject.days[i].datetime,
+        icon: weatherJsonObject.days[i].icon,
+        temp: weatherJsonObject.days[i].temp,
+      });
+    }
+
+    return weekArr;
+  }
+
+  getIcon() {
+    return this.icon;
+  }
+
+  getTemp() {
+    return this.temp;
+  }
+
+  getHumidity() {
+    return this.humidity;
+  }
+  getDew() {
+    return this.dew;
+  }
+  getCondition() {
+    return this.condition;
+  }
+  getSunriseTime() {
+    return this.sunriseTime;
+  }
+  getWindspeed() {
+    return this.windspeed;
+  }
+  getCloudCover() {
+    return this.cloudcover;
+  }
+  getWeekArr() {
+    return this.weekArr;
+  }
+}
+
 function constructURL(location, temperatureUnit) {
   const farenheitUnitGroup = "us";
   const celsiusUnitGroup = "metric";
@@ -15,23 +76,12 @@ async function fetchWeather(url) {
   return json;
 }
 
-async function getWeather(location = `New Delhi`, temperatureUnit = "C") {
+async function getWeather(location = `Delhi`, temperatureUnit = "C") {
   const url = constructURL(location, temperatureUnit);
+  console.log(url);
   const weatherInfo = await fetchWeather(url);
 
-  const weatherObj = weatherInfo;
-  const testObj = {
-    icon: weatherObj.currentConditions.icon,
-    temp: weatherObj.currentConditions.temp,
-    humidity: weatherObj.currentConditions.humidity,
-    dew: weatherObj.currentConditions.dew,
-    condition: weatherObj.currentConditions.conditions,
-    sunriseTime: weatherObj.currentConditions.sunrise,
-    sunsetTime: weatherObj.currentConditions.sunset,
-    windspeed: weatherObj.currentConditions.windspeed,
-    cloudcover: weatherObj.currentConditions.cloudcover,
-    weekArr: weatherObj.days,
-  };
+  const weatherObj = new WeatherInformation(weatherInfo);
 }
 
-export { fetchWeather };
+export { getWeather };
