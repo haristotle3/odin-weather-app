848ad9049830e83a28e528ea226b53995da0c2a6
haristotle <haristotlepatil2003@gmail.com>
2025-07-18 19:42:51 +0530

    Refactor getInitWeather in localStorageHandler.js

--- src/localStorageHandler.js
+++ src/localStorageHandler.js
@@ -1,16 +1,16 @@
 import { getWeather } from "./weatherAPI";
 
+function updateLocalStorage(newWeather) {
+  localStorage.setItem("defaultWeather", JSON.stringify(newWeather));
+  return;
+}
+
 export default async function getInitWeather() {
   if (!localStorage.getItem("defaultWeather")) {
     const currentWeather = await getWeather("Delhi");
-    localStorage.setItem("defaultWeather", JSON.stringify(currentWeather));
+    updateLocalStorage(currentWeather);
     return currentWeather;
   }
   const initWeather = JSON.parse(localStorage.getItem("defaultWeather"));
   return initWeather;
-}
-
-function updateLocalStorage(newWeather) {
-  localStorage.setItem("defaultWeather", JSON.stringify(newWeather));
-  return;
 }
